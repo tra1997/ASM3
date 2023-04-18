868d497908bad91eed56df0ee28d463d5306460d
tra1997 <128974971+tra1997@users.noreply.github.com>
2023-04-18 23:49:06 +0700

    Add files via upload

--- viewmore.js
+++ viewmore.js
@@ -1,13 +1,14 @@
 const viewMoreBtns = document.querySelectorAll(".view-more");
-      viewMoreBtns.forEach((btn) => {
-        btn.addEventListener("click", () => {
-          const jobContent = btn.nextElementSibling;
-          if (jobContent.classList.contains("hide")) {
-            jobContent.classList.remove("hide");
-            btn.textContent = "View Less";
-          } else {
-            jobContent.classList.add("hide");
-            btn.textContent = "View More";
-          }
-        });
-      });
+viewMoreBtns.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    const jobContent = btn.nextElementSibling;
+    console.log(jobContent);
+    if (jobContent.classList.contains("hide")) {
+      jobContent.classList.remove("hide");
+      btn.textContent = "View Less";
+    } else {
+      jobContent.classList.add("hide");
+      btn.textContent = "View More";
+    }
+  });
+});
